kerneltoast/android_kernel_google_zuma

aoc: Fix probe error handling
Numerous resources may not be cleaned up when probing fails. And worse, an
error may not be returned sometimes when probing fails.

Fix up the probe error handling to clean up everything appropriately, and
ensure that an error is always returned when probing fails.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

google-modules/aoc/aoc.c
@@ -2324,7 +2324,7 @@ static int aoc_platform_probe(struct platform_device *pdev)
 		sizeof(struct mbox_slot) * prvdata->aoc_mbox_channels, GFP_KERNEL);
 	if (!prvdata->mbox_channels) {
 		rc = -ENOMEM;
-		goto err_failed_prvdata_alloc;
+		goto err_invalid_dt;
 	}
 
 	prvdata->dev = dev;
@@ -2340,7 +2340,7 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	if (rc) {
 		dev_err(dev, "Failed to initialize gsa device: %d\n", rc);
 		rc = -EINVAL;
-		goto err_failed_prvdata_alloc;
+		goto err_invalid_dt;
 	}
 
 	ret = init_chardev(prvdata);
@@ -2397,8 +2397,8 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	init_waitqueue_head(&prvdata->aoc_reset_wait_queue);
 	INIT_WORK(&prvdata->watchdog_work, aoc_watchdog);
 
-	ret = configure_watchdog_interrupt(pdev, prvdata);
-	if (ret < 0)
+	rc = configure_watchdog_interrupt(pdev, prvdata);
+	if (rc < 0)
 		goto err_watchdog_irq;
 
 	sysmmu_node = of_parse_phandle(aoc_node, "iommus", 0);
@@ -2407,8 +2407,8 @@ static int aoc_platform_probe(struct platform_device *pdev)
 		rc = -ENODEV;
 		goto err_watchdog_sysmmu_irq;
 	}
-	ret = configure_sysmmu_interrupts(dev, sysmmu_node, prvdata);
-	if (ret < 0)
+	rc = configure_sysmmu_interrupts(dev, sysmmu_node, prvdata);
+	if (rc < 0)
 		goto err_watchdog_sysmmu_irq;
 	of_node_put(sysmmu_node);
 
@@ -2546,8 +2546,11 @@ err_mem_resources:
 err_memnode:
 	deinit_chardev(prvdata);
 err_chardev:
-err_failed_prvdata_alloc:
+	devm_remove_action(dev, release_gsa_device, prvdata);
 err_invalid_dt:
+	platform_set_drvdata(pdev, NULL);
+	devm_kfree(dev, prvdata);
+err_failed_prvdata_alloc:
 	aoc_platform_device = NULL;
 err_platform_not_null:
 	return rc;

Trijal08/anaconda_kernel-msm-6.1

google-modules/aoc/aoc.c
@@ -2572,7 +2572,7 @@ static int aoc_platform_probe(struct platform_device *pdev)
 		sizeof(struct mbox_slot) * prvdata->aoc_mbox_channels, GFP_KERNEL);
 	if (!prvdata->mbox_channels) {
 		rc = -ENOMEM;
-		goto err_failed_prvdata_alloc;
+		goto err_invalid_dt;
 	}
 
 	prvdata->dev = dev;
@@ -2588,7 +2588,7 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	if (rc) {
 		dev_err(dev, "Failed to initialize gsa device: %d\n", rc);
 		rc = -EINVAL;
-		goto err_failed_prvdata_alloc;
+		goto err_invalid_dt;
 	}
 
 	ret = init_chardev(prvdata);
@@ -2645,8 +2645,8 @@ static int aoc_platform_probe(struct platform_device *pdev)
 	init_waitqueue_head(&prvdata->aoc_reset_wait_queue);
 	INIT_WORK(&prvdata->watchdog_work, aoc_watchdog);
 
-	ret = configure_watchdog_interrupt(pdev, prvdata);
-	if (ret < 0)
+	rc = configure_watchdog_interrupt(pdev, prvdata);
+	if (rc < 0)
 		goto err_watchdog_irq;
 
 	iommu_node = of_parse_phandle(aoc_node, "iommus", 0);
@@ -2655,8 +2655,8 @@ static int aoc_platform_probe(struct platform_device *pdev)
 		rc = -ENODEV;
 		goto err_watchdog_iommu_irq;
 	}
-	ret = configure_iommu_interrupts(dev, iommu_node, prvdata);
-	if (ret < 0)
+	rc = configure_iommu_interrupts(dev, iommu_node, prvdata);
+	if (rc < 0)
 		goto err_watchdog_iommu_irq;
 	of_node_put(iommu_node);
 
@@ -2784,8 +2784,11 @@ err_mem_resources:
 err_memnode:
 	deinit_chardev(prvdata);
 err_chardev:
-err_failed_prvdata_alloc:
+	devm_remove_action(dev, release_gsa_device, prvdata);
 err_invalid_dt:
+	platform_set_drvdata(pdev, NULL);
+	devm_kfree(dev, prvdata);
+err_failed_prvdata_alloc:
 	aoc_platform_device = NULL;
 err_platform_not_null:
 	return rc;