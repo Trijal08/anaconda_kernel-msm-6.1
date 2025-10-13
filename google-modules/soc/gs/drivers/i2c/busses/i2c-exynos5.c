kerneltoast/android_kernel_google_gs201

i2c: exynos5: Set IRQF_NOBALANCING
IRQ balancing is already performed naturally by moving the i2c IRQ to the
CPU that kicks off an i2c transaction. Therefore, opt out from IRQ
balancing operations by setting IRQF_NOBALANCING.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

drivers/i2c/busses/i2c-exynos5.c
@@ -1302,8 +1302,8 @@ static int exynos5_i2c_probe(struct platform_device *pdev)
 		}
 
 		ret = devm_request_irq(&pdev->dev, i2c->irq, exynos5_i2c_irq,
-				       IRQF_NO_THREAD, dev_name(&pdev->dev),
-				       i2c);
+				       IRQF_NO_THREAD | IRQF_NOBALANCING,
+				       dev_name(&pdev->dev), i2c);
 		disable_irq(i2c->irq);
 
 		if (ret != 0) {

Trijal08/anaconda_kernel-msm-6.1

google-modules/soc/gs/drivers/i2c/busses/i2c-exynos5.c
@@ -1346,7 +1346,8 @@ static int exynos5_i2c_probe(struct platform_device *pdev)
 		}
 
 		ret = devm_request_irq(&pdev->dev, i2c->irq, exynos5_i2c_irq,
-					IRQF_NO_SUSPEND, dev_name(&pdev->dev), i2c);
+				       IRQF_NO_SUSPEND | IRQF_NOBALANCING,
+				       dev_name(&pdev->dev), i2c);
 		disable_irq(i2c->irq);
 
 		if (ret != 0) {